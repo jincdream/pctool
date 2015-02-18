2e2a4aa57def27cc237e966fea0c23b35dd737bd
jincdream <275727449@qq.com>
2015-02-18 19:48:06 +0800

    @Jin_C : http server ,support gbk and utf-8.
    @Jin_C : http server ,support gbk and utf-8.
      @param
       --> ip @number
       --> port @number
       --> [true,false,'all'] @bullen or string
      @return undefined

--- src/httpServer/index.js
+++ src/httpServer/index.js
@@ -4,10 +4,10 @@ var url = require('url');
 var mime = require('mime');
 var iconv = require('iconv-lite');
 var server = exports = {};
-var readGbk = require('../readGbk');
 var dirName = ph.resolve()
 var gbk = {gbk:!1}
-
+var lst = new require('events').EventEmitter
+console.log(lst);
 server.http = function(req,res){
 		/**
 		* url obj
@@ -37,30 +37,78 @@ server.http = function(req,res){
 console.log(dirName);
 
 
+server.readGbk = require('../readGbk');
+
 server.checkGbk = function(string){
 	return string.indexOf('�') >= 0
 }
 
 server.render = function(path,req,res){
-	var type = mime.lookup(path) || 'test/html';
+	var startT = +new Date
+	var type = mime.lookup(path) || 'text/html';
 	var oType = {
 		'Content-Type' : type,
 		'Access-Control-Allow-Origin':'*'
 	};
 	var fileName = ph.join(dirName,path)
 	var read,write
-	if(!server.gbk){
-		read = fs.createReadStream(fileName)
-		write = fs.createWriteStream('a')
-		read.on('data',function(data){
+	var _readed = !0
+	read = fs.createReadStream(fileName)
+	write = fs.createWriteStream('a')
 
+	if(server.gbk === 'all'){
+		read.pipe(write)
+
+		read.on('data',function(data){
+			if(!_readed)return
+			if(server.checkGbk(data.toString())){
+				console.log('ggggggggggggg');
+				oType['Content-Type'] += ';charset=gbk'
+				res.writeHead(200,oType);
+				server.readGbk(fileName).pipe(res)
+				_readed = !1
+				console.log(+new Date - startT);
+			}
 		})
-		if(server.checkGbk(read)){
-			read = readGbk(fileName,req,res)
-			oType['Content-Type'] += ';charset=gbk'
-			console.log('gbk');
-		}
+		read.on('end',function(a){
+			if(_readed){
+				oType['Content-Type'] += ';charset=utf-8'
+				res.writeHead(200,oType);
+				fs.createReadStream(fileName).pipe(res)
+				console.log('8888888888888888');
+				console.log(+new Date - startT);
+			}
+		})
+	}else if(server.gbk === 'utf-8'){
+		oType['Content-Type'] += ';charset=utf-8'
+		res.writeHead(200,oType);
+		console.log(read)
+		read.pipe(res)
+		console.log('server.gbk --- %s',server.gbk)
+		_readed = !1
+		console.log(+new Date - startT);
+	}else if(server.gbk === 'gbk'){
+		oType['Content-Type'] += ';charset=gbk'
+		res.writeHead(200,oType);
+		server.readGbk(fileName).pipe(res)
+		_readed = !1
+		console.log(+new Date - startT);
 	}
+
+
+	// if(!server.gbk){
+	// 	read = fs.createReadStream(fileName)
+	// 	write = fs.createWriteStream('a')
+	// 	read.on('data',function(data){
+	//
+	// 	})
+	//
+	// }else if(server.checkGbk(read)){
+	// 		read = readGbk(fileName,req,res)
+	// 		oType['Content-Type'] += ';charset=gbk'
+	// 		console.log('gbk');
+	//
+	// }
 	// if(!!~req.url.indexOf('gbk')){
 		// read = readGbk(ph.join(dirName,path),req,res)
 		// oType['Content-Type'] += ';charset=gbk'
@@ -71,17 +119,18 @@ server.render = function(path,req,res){
 	// 	read = fs.createReadStream('.' + path,oType.char)
 	// }
 
-	res.writeHead(200,oType);
+
 	read.on('error',function(err){
 		console.log(err)
 		res.writeHead(404,oType)
 		res.end(new Buffer('404啦！funk'))
 	})
-	read.pipe(res);
+	// read.pipe(res);
 };
 
 // iconv.extendNodeEncodings();
-module.exports = function(host,port){
+module.exports = function(host,port,gbk){
+	server.gbk = gbk || 'utf-8'
 	require('http').createServer(server.http).listen(port,host || '127.0.0.1');
 	console.log(host+ ':' + port);
 	return server
--- test/httpServer.js
+++ test/httpServer.js
@@ -5,5 +5,12 @@ var ip = (function () {
   _host = !!_host ? interwork.en0 : interwork['本地连接'];//windows
   return _host[1].address;
 })();
-
-http(ip,10086)
+/*
+  @Jin_C : http server ,support gbk and utf-8.
+  @param
+   --> ip @number
+   --> port @number
+   --> [true,false,'all'] @bullen or string
+  @return undefined
+*/
+http(ip,10086,'all')
