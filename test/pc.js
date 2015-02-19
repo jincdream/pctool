ca91c3a838c8fb84495aedbe7c12b0d4d418ce1d
jincdream <275727449@qq.com>
2015-02-20 00:41:45 +0800

    O(∩_∩)O谢谢

--- test/pc.js
+++ test/pc.js
@@ -7,29 +7,70 @@ var server = child.fork('../src/rServer');
 var httpServer = require('../src/httpServer');
 var readDirSync = require('../src/readdirSync');
 var changeBug = 0
-console.log(ph.resolve(),' -pc');
+var log = console.log
+
+Object.prototype.log = function(name){
+  log.call(console,this[name],' -' + name)
+}
+
+console.log = function(object,tag){
+  var str = ''
+  var _tag = ''
+  arguments
+  if(Array.isArray(object)){
+    _tag = 'isArray'
+  }else{
+    _tag = object.name
+  }
+  log.apply(console,[object,'  -' + (tag || _tag)])
+}
+console.log(ph.resolve(),'pc');
 /*
   @Jin_C :
+
   @Param : option @Object
     -->{
       target: 'target dir src' ,
-      [output: 'output dir name' || 'output' ,]
-
+      [output: 'output dir name' || './output' ,]
+        --> !! --> option.target = ph.join(ph.resolve(),option.target)
     }
 */
 var pcTool = function(option){
   var _self = this
-  fs.exists(option.target,function(exs){
-    if(!exs){
-
-    }else{
-
-    }
-  })
+  option.target = ph.join(ph.resolve(),option.target)
+  option.log('target')
   for(var n in option){
     _self[n] = option[n]
   }
-  _self.watchDir()
+  _self.output = _self.path(option.output || './output')
+  // console.log(option.target + ' -target');
+
+  fs.exists(option.target,function(exs){
+    if(!exs){
+      _self.initDir(_self.watchDir)
+    }else{
+      _self.watchDir()
+    }
+  })
+}
+pcTool.prototype.path = function(src,target){
+  return ph.join(target || this.target,src)
+}
+pcTool.prototype.initDir = function(next){
+  var _self = this
+  _self.outputSrc = _self.path('./output')
+  _self.moduleSrc = _self.path('./module')
+  fs.mkdirSync(_self.target)
+  fs.mkdir(_self.outputSrc,function(err){
+    if(err) throw new Error(err)
+    fs.createWriteStream(_self.path('./a.html'))
+
+    next && next.call(_self)
+    // fs.mkdir()
+  })
+  fs.mkdir(_self.moduleSrc,function(err){
+    if(err) throw new Error(err)
+  })
 }
 pcTool.prototype.readDirSync = function(handle){
   var _self = this
@@ -70,7 +111,16 @@ pcTool.prototype.watchHandle = function(path,eventN,file){
 module.exports = function(option){
   new pcTool(option)
 }
-new pcTool({
-  target: './src/',
+/*
+  @Jin_C :
+  @Param : option @Object
+    -->{
+      target: 'target dir src' ,
+      [output: 'output dir name' || 'output' ,]
+
+    }
+*/
+;new pcTool({
+  target: './ttt/',
   ignore: ['ig']
 })
